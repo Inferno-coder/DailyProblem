a77c9c6333b7c7c63d1145446863fdfed82f7f8a
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-05-28 21:17:44 +0530

    Create NthNodeFromTheLast.cpp

---
+++ NthNodeFromTheLast.cpp
@@ -0,0 +1,18 @@
+int getNthFromLast(Node *head, int n)
+    {
+          Node*temp= head;
+          int c=0;
+          while(temp!=0)
+          {
+              c++;
+              temp=temp->next;
+          }
+          temp=head;
+          if(n>c)
+          return -1;
+          for(int i=0;i<(c-n);i++)
+          {
+              temp=temp->next;
+          }
+          return temp->data;
+    }
