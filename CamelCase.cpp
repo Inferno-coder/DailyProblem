683a16515dca1560c76fdb4e58a6238cda3caf13
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-05-29 22:33:44 +0530

    Create CamelCase.cpp

---
+++ CamelCase.cpp
@@ -0,0 +1,21 @@
+    vector<string> CamelCase(int N, vector<string> Dictionary, string Pattern) {
+        // code here
+        vector<string> up;
+        
+        for(auto x: Dictionary){
+            string st;
+            st.clear();
+            for(auto s: x){
+                if(isupper(s))
+                       st+=s; 
+                    
+            }
+            if(Pattern==string(st.begin() + 0, st.begin() + Pattern.length()))
+                up.push_back(x);
+        }
+        
+        if(up.size() ==0)
+            return {"-1"};
+        
+        return up;
+    }
