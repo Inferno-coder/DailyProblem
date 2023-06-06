f4e0bfe191b1eed68b7b68e9f39c6311eb73bff9
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-06-06 22:52:46 +0530

    Create AP.cpp

---
+++ AP.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int diff=arr[1]-arr[0];
+        for(auto &it:arr)cout<<it<<" ";
+        for(int i=2;i<arr.size();i++){
+        int temp=arr[i]-arr[i-1];
+        if(temp!=diff)return false;
+        }
+        return true;
+    }
+};
