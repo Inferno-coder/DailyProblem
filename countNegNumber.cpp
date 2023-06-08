90cd7846ce1b8f27d55f8f46668a23df33fa2cbb
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-06-08 22:13:57 +0530

    Create countNegNumber.cpp

---
+++ countNegNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int count=0;
+        for(int i=0;i<grid.size();i++){
+            vector<int>numbers=grid[i];
+           auto it = std::upper_bound(numbers.begin(), numbers.end(), 0, std::greater<int>());
+           if(it!=numbers.end()){
+               int dist=distance(numbers.begin(),it);
+               count+=(grid[0].size()-dist);
+           }
+        }
+        return count;
+    }
+};
