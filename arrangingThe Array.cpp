e5e31d322637fc7427e8e7ae6d01f3f504ac36e6
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-06-10 23:03:52 +0530

    Create arrangingThe Array.cpp

---
+++ arrangingThe Array.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            // Your code goes here
+            vector<int>temp;
+            for(int i=0;i<n;i++){
+                if(arr[i]>=0)temp.push_back(arr[i]);
+            }
+            int a=0;
+            for(int i=0;i<n;i++){
+                if(arr[i]<0)arr[a++]=arr[i];
+            }
+            for(int i=0;i<temp.size();i++){
+                arr[a++]=temp[i];
+            }
+        }
+};
