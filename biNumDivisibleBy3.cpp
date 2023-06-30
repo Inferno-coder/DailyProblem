d3b928ec4f02922f317204956d5a6c55d2c5e43a
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-06-30 22:16:53 +0530

    Create biNumDivisibleBy3.cpp

---
+++ biNumDivisibleBy3.cpp
@@ -0,0 +1,17 @@
+class Solution{
+public:	
+		
+	int isDivisible(string s){
+	    //complete the function here
+	    int even=0;
+	    int odd=0;
+	    for(int i=0;i<s.length();i++){
+	        if(s[i]=='1'){
+	            if(i%2)odd++;
+	            else even++;
+	        }
+	    }
+	    return abs(even-odd)%3==0;
+	}
+
+};
