c6fea69ef50a49224f8a560cfc732bf01ebe4445
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-07-06 23:09:53 +0530

    Update countNegNumber.cpp

--- countNegNumber.cpp
+++ countNegNumber.cpp
@@ -5,9 +5,9 @@ public:
         int count=0;
         for(int i=0;i<grid.size();i++){
             vector<int>numbers=grid[i];
+            
            auto it = std::upper_bound(numbers.begin(), numbers.end(), 0, std::greater<int>());
            if(it!=numbers.end()){
-               
                int dist=distance(numbers.begin(),it);
                count+=(grid[0].size()-dist);
            }
