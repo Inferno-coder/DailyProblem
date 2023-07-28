e35a56d8482ae9fdc177fc0c52995bdaa35ab4ef
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-07-28 22:10:24 +0530

    Create LCA.cpp

---
+++ LCA.cpp
@@ -0,0 +1,20 @@
+Node* LCA(Node *root, int p, int q)
+        {
+            // code here
+            if (root == NULL || root->data == p || root->data == q) {
+            return root;
+        }
+        Node* left = LCA(root->left, p, q);
+        Node* right = LCA(root->right, p, q);
+
+        //result
+        if(left == NULL) {
+            return right;
+        }
+        else if(right == NULL) {
+            return left;
+        }
+        else { //both left and right are not null, we found our result
+            return root;
+        }
+        }
