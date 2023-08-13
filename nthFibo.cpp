b829a5ab62356ab24aa7004a8a9c7629b5b797bb
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-08-13 22:37:53 +0530

    Create nthFibo.cpp

---
+++ nthFibo.cpp
@@ -0,0 +1,13 @@
+  public:
+    int nthFibonacci(int n){
+        // code here
+          int dp[n+1];
+     dp[0] = 0;
+     dp[1] = 1;
+     
+     for(int i=2;i<=n;i++){
+         
+         dp[i] = dp[i-1]%1000000007 + dp[i-2]%1000000007;
+     }
+     return dp[n]%1000000007;
+    }
