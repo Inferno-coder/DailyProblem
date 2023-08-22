dbe001060d76285410a7d4f12795c7b6f217a040
Inferno-coder <98871228+Inferno-coder@users.noreply.github.com>
2023-08-22 23:40:21 +0530

    Create convertToTitle.cpp

---
+++ convertTo/convertToTitle.cpp
@@ -0,0 +1,8 @@
+std::string convertToTitle(int columnNumber) {
+        std::string out;
+        while (columnNumber > 0) {
+            out = char('A' + (columnNumber - 1) % 26) + out;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        return out;
+    }
