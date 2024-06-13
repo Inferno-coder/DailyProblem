1ed40453bc3dc1dbe1b8fff468e7d44ca36f68ea
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2024-06-13 22:34:58 +0530

    Create padovanSequence.cpp

---
+++ padovanSequence.cpp
@@ -0,0 +1,19 @@
+int padovanSequence(int n)
+    {
+       //code here
+           if(n<=2)
+       {
+           return 1;
+       }
+       int a=1,b=1,c=1;
+       
+       for(int i=3;i<=n;i++)
+       {
+           int temp=(a+b)%1000000007;
+           a=b;
+           b=c;
+           c=temp;
+       }
+       
+       return c;
+    }
